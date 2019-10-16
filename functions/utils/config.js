558ae5f7fefaa3ad77ab369bc7c343427d17b648
Nzubechukwu <nzubennamani@gmail.com>
2019-10-16 08:29:11 +0100

    refactored codebase

---
+++ functions/utils/config.js
@@ -0,0 +1,11 @@
+module.exports = firebaseConfig = {
+    apiKey: "AIzaSyDPubHGXUbaMFEaydLzvdA_yW94x6q3Gto",
+    authDomain: "konnect-8dd5c.firebaseapp.com",
+    databaseURL: "https://konnect-8dd5c.firebaseio.com",
+    projectId: "konnect-8dd5c",
+    storageBucket: "konnect-8dd5c.appspot.com",
+    messagingSenderId: "184211229322",
+    appId: "1:184211229322:web:ca6d0dc97dc628f92b3b73",
+    measurementId: "G-BKK3142WGQ"
+  };
+
