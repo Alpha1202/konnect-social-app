219101ca46f874dfb9252ca0e42814515eff9ca4
Nzubechukwu <nzubennamani@gmail.com>
2019-12-30 18:56:59 +0100

    get one scream

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,23 @@
-require('dotenv').config()
+require('dotenv').config();
 const functions = require('firebase-functions');
 
 const app = require('express')();
 
-const FBAuth = require('./utils/fbAuth')
+const FBAuth = require('./utils/fbAuth');
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
+const { getAllScreams, postOneScream, getScream } = require('./handlers/screams');
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 
-
 // Get All Screams route
-app.get('/screams', getAllScreams)
+app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
 
 // Users route
-app.post('/signup', signup); 
+app.post('/signup', signup);
 app.post('/login', login);
-app.post('/user/image', FBAuth, uploadImage)
-app.post('/user', FBAuth, addUserDetails)
-app.get('/user', FBAuth, getAuthenticatedUser)
-
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
